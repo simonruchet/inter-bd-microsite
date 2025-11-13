5c13da78d5fe834778c673477e124652b9433157
simonruchet <simon.ruchet@studentfr.ch>
2025-11-13 10:10:01 +0100

    debut parallaxe

--- src/js/immersion.js
+++ src/js/immersion.js
@@ -3,41 +3,44 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// Animation pour la montagne
+//  scroll horizontal
 
-// gsap
-//   .timeline({
-//     scrollTrigger: {
-//       trigger: ".mountain-in",
-//       start: "top top",
-//       end: "+=1000", // longueur du scroll
-//       scrub: true,
-//       pin: true, // bloque la section pendant l'anim
-//     },
-//   })
-//   .to(".top", { y: "-100%", ease: "none" }, 0)
-//   .to(".bottom", { y: "100%", ease: "none" }, 0)
-//   .to(
-//     ".mountain-in-case",
-//     {
-//       width: 793,
-//       height: 557,
-//       ease: "none",
-//     },
-//     0
-//   );
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".scroll-horizontal",
+      start: "top top",
+      end: "+=1400%",
+      pin: true,
+      scrub: true,
+      markers: true,
+    },
+  })
 
-// Animation pour le scroll horizontal
+  // arrêt sur le screen 3
+  .to(".screens", {
+    x: "-14.28%",
+    ease: "sine.inOut",
+  })
 
-gsap.to(".screens", {
-  scrollTrigger: {
-    trigger: ".scroll-horizontal",
-    start: "top top",
-    end: "+=1400%",
-    pin: true,
-    scrub: true,
-    markers: true,
-  },
-  x: "-92.8571%",
-  ease: "sine.inOut",
-});
+  // parallax
+  .to(".screen-3-background", {
+    x: "-8vw",
+    ease: "quad.inOut",
+  })
+
+  .to(".screen-3-trees", {
+    x: "-10vw",
+    ease: "quad.inOut",
+  })
+
+  .to(".screen-3-sabre", {
+    x: "5vw",
+    ease: "quad.inOut",
+  })
+
+  // retour scroll horizontal
+  .to(".screens", {
+    x: "-92.8571%",
+    ease: "sine.inOut",
+  });
